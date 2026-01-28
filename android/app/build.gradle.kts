e232eb67c183638510704649f9239052eacac805
amarhumayunx <amarhumayun@outlook.com>
2026-01-28 20:04:50 +0500

    Change package name to shopping_catalog_app and com.shoppingcatalog.app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.shoppingcatalogapp"
+    namespace = "com.shoppingcatalog.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.shoppingcatalogapp"
+        applicationId = "com.shoppingcatalog.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shoppingcatalogapp/main.dart';
+import 'package:shopping_catalog_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
